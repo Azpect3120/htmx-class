3fb217a47646c39bb62a6d8d9025b506f35c1050
Azpect3120 <104033825+Azpect3120@users.noreply.github.com>
2024-04-02 16:29:12 -0700

    FEAT: Duplicates will not be allowed!

--- server/server.js
+++ server/server.js
@@ -15,30 +15,34 @@ let contacts = [];
 let lastId = 0;
 
 app.get("/", (req, res) => {
-    logger.log(req);
-    res.status(200).render("index", {contacts});
+  logger.log(req);
+  res.status(200).render("index", {contacts});
 });
 
 app.post("/contacts", (req, res) => {
-    logger.log(req);
-    let { name, email } = req.body;
-    let id = lastId++;
-    contacts.push({id, name, email});
-    res.status(201).render("templates/contact", { id, name, email });
+  logger.log(req);
+  let { name, email } = req.body;
+  if (contacts.find(contact => contact.email === email)) {
+    res.status(409).render("templates/error");
+    return
+  }
+  let id = lastId++;
+  contacts.push({id, name, email});
+  res.status(201).render("templates/contact", { id, name, email });
 });
 
 app.get("/contacts", (req, res) => {
-    logger.log(req);
-    res.status(200).json(contacts);
+  logger.log(req);
+  res.status(200).json(contacts);
 });
 
 app.delete("/contacts/:id", (req, res) => {
-    logger.log(req);
+  logger.log(req);
 
-    const id = req.params.id;
-    contacts = contacts.filter(contact => contact.id != id);
+  const id = req.params.id;
+  contacts = contacts.filter(contact => contact.id != id);
 
-    res.status(204).send("");
+  res.status(204).send("");
 });
 
 
